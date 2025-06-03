2433a6b3c8f17f06695a1eeda06b38dfa328dcc1
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-03 18:04:49 +0200

    update

--- libft_master.c
+++ libft_master.c
@@ -72,14 +72,3 @@ LibFunc get_function_by_index(int index) {
 	}
 	return functions[index];
 };
-
-void get_user_input(char *buffer, size_t size) {
-	printf("\nStart typing below. End with a single line containing only 'END'\n\n");
-	buffer[0] = '\0';
-	char line[1024];
-	while (fgets(line, sizeof(line), stdin)) {
-		if (strcmp(line, "END\n") == 0 || strcmp(line, "END\r\n") == 0)
-			break;
-		strncat(buffer, line, size - strlen(buffer) - 1);
-	}
-}
