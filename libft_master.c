02580234cd83cb8de5e5f3ef75ecce62ba11ad81
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-03 17:58:21 +0200

    update

--- libft_master.c
+++ libft_master.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "libft_master.h"
+#include "./includes/libft_master.h"
 
 #define FUNC_COUNT (get_string_function_count() + get_memory_function_count())
 
