6d2b6cad47f40a7241bb2685807b8f731bfb48b4
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-03 18:07:17 +0200

    update

--- libft_master.c
+++ libft_master.c
@@ -6,10 +6,3 @@
 
 #define FUNC_COUNT (get_string_function_count() + get_memory_function_count())
 
-void print_mode_menu(void) {
-	printf("Choose a mode:\n");
-	printf("1. Copy Mode   (see function code and type it)\n");
-	printf("2. Recall Mode (type function from memory)\n");
-	printf("Your choice: ");
-}
-
