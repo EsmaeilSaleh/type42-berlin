ab23aebcf578d1336fe04a7386d94d5336c9b704
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-04 06:21:15 +0200

    update

--- src/app/utils.c
+++ src/app/utils.c
@@ -20,3 +20,24 @@ void print_function_list(LibFunc (*get_func_by_index)(int), int count) {
 		printf("%d. %s\n", i + 1, f.name);
 	}
 }
+void get_user_input(char *buffer, size_t size)
+{
+	char line[1024];
+	buffer[0] = '\0';
+
+	printf("\nStart typing below. Type 'END' on a line to finish.\n\n");
+
+	while (fgets(line, sizeof(line), stdin))
+	{
+		if (strcmp(line, "END\n") == 0 || strcmp(line, "END\r\n") == 0)
+			break;
+
+		if (strlen(buffer) + strlen(line) + 1 >= size)
+		{
+			fprintf(stderr, "Input buffer full. Truncating input.\n");
+			break;
+		}
+
+		strncat(buffer, line, size - strlen(buffer) - 1);
+	}
+}
