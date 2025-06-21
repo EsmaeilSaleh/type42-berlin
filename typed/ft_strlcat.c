44fe7e15e3921241d46c234d3695806c321712ef
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-21 16:46:25 +0200

    log updated

--- typed/ft_strlcat.c
+++ typed/ft_strlcat.c
@@ -3,28 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: type42 <esaleh@student.42.de>              +#+  +:+       +#+        */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/20 20:00:00 by type42            #+#    #+#             */
-/*   Updated: 2025/06/20 20:00:00 by type42           ###   ########.fr       */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: ft_strlcat — Practice Mode: Copy / Recall
+*/
+
+size_t	ft_strlcat(cahr *dst, const char *src, size_t dstsize)
 {
-	size_t dst_len;
-	size_t src_len;
-	size_t i;
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
 
 	dst_len = 0;
 	while (dst[dst_len] && dst_len < dstsize)
 		dst_len++;
-	src_len = 0;
-	while (src[src_len])
-		src_len++;
 	if (dst_len == dstsize)
 		return (dstsize + src_len);
-	i = 0;
 	while (src[i] && (dst_len + i) < (dstsize - 1))
 	{
 		dst[dst_len + i] = src[i];
