f0c2653138f03c3d79d166bd131b8c7fdcc6f0f4
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-21 10:46:01 +0200

    log updated

---
+++ typed/ft_bzero.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: ft_bzero — Practice Mode: Copy / Recall
+*/
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)s;
+	while (n--)
+		*p++ = 0;
+}
