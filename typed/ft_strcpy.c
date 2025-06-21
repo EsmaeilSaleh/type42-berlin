4261c4ae5fa6215b9a555b55ff738cdf9389f92e
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-21 13:08:17 +0200

    log updated

--- typed/ft_strcpy.c
+++ typed/ft_strcpy.c
@@ -3,20 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: type42 <esaleh@student.42.de>              +#+  +:+       +#+        */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/20 20:00:00 by type42            #+#    #+#             */
-/*   Updated: 2025/06/20 20:00:00 by type42           ###   ########.fr       */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: ft_strcpy — Practice Mode: Copy / Recall
+*/
+
 char	*ft_strcpy(char *dst, const char *src)
 {
-	char	*ret;
+	size_t	i;
 
-	ret = dst;
-	while (*src)
-		*dst++ = *src++;
-	*dst = '\0';
-	return (ret);
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	+
+	dst[i] = '\0';
+	return (dst);
 }
