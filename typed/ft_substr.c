bbb21d9104fe359df082eb5963b6aa772e9ae048
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-21 19:13:59 +0200

    log updated

--- typed/ft_substr.c
+++ typed/ft_substr.c
@@ -15,28 +15,28 @@
 **  Function: ft_substr — Practice Mode: Copy / Recall
 */
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *src, unsigned int start, size_t len)
 {
-	char	*sub;
-	size_t	i;
-	size_t	s_len;
+	char	*substr;
+	size_t	index;
+	size_t	src_len;
 
-	if (!s)
+	if (!src)
 		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
+	src_len = ft_strlen(src);
+	if (start >= src_len)
 		return (ft_strdup(""));
-	if (len > (s_len - start)
-		len = s_len - start;
-	sub = malloc(len + 1);
-	if (!sub)
+	if (len > (src_len - start))
+		len = src_len - start;
+	substr = malloc(len + 1);
+	if (!substr)
 		return (NULL);
-	i = 0;
-	while (i < len && s[start + i])
+	index = 0;
+	while (index < len && src[start + index])
 	{
-		sub[i] = s[start + i];
-		i++;
+		substr[index] = src[start + index];
+		index++;
 	}
-	sub[i] = '\0';
-	return (sub);
+	substr[index] = '\0';
+	return (substr);
 }
