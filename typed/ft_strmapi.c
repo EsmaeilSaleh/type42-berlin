7080d21afcbbe0ab29700999e3e143ae85746a12
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-21 19:39:57 +0200

    log updated

--- typed/ft_strmapi.c
+++ typed/ft_strmapi.c
@@ -15,22 +15,3 @@
 **  Function: ft_strmapi — Practice Mode: Copy / Recall
 */
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	size_t	i;
-	char	*result;
-
-	if (!s || !f)
-		return (NULL);
-	result = malloc(ft_strlen(s) + 1);
-	if (!result)
-		return (NULL);
-	i = 0;
-	while (s[i])
-	{
-		result[i] = f(i, s[i]);
-		i++;
-	}
-	result[i] = '\0';
-	return (result);
-}
