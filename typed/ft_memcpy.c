cbc1e9e9051b7e6237aeba3fa650c83ba1e2f154
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-21 10:47:44 +0200

    log updated

--- typed/ft_memcpy.c
+++ typed/ft_memcpy.c
@@ -17,19 +17,12 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t					i;
-	unsigned char			*d;
-	const unsigned char		*s;
+	unsigned char		*d = (unsigned char *)dest;
+	const unsigned char	*s = (const unsigned char *)src;
 
-	if (!dest && !src)
+	if (!d && !s)
 		return (NULL);
-	d = (unsigned char *)dest;
-	s = (const unsigned char *)src;
-	i = 0;
-	while (i < n)
-	{
-		d[i] = s[i];
-		i++;
-	}
+	while (n--)
+		*d++ = *s++;
 	return (dest);
 }
