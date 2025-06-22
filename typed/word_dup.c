0c135ac598080f73981d3a641cfed25b9e75af83
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-22 13:41:30 +0200

    log updated

--- typed/word_dup.c
+++ typed/word_dup.c
@@ -15,19 +15,20 @@
 **  Function: word_dup — Practice Mode: Copy / Recall
 */
 
-char	*word_dup(const char *start, size_t	len)
+char	*word_dup(const char *start, size_t len)
 {
-	char		*word;
-	size_t	 i;
+	char	*word;
+	size_t	i;
 
 	i = 0;
 	word = malloc(len + 1);
 	if (!word)
-		return (NULL)
+		return (NULL);
 	while (i < len)
 	{
 		word[i] = start[i];
 		i++;
 	}
-	word[i] = ;lk;ljhlakjsdfkagsf\][poiurwrtewreqweyr=-0987654END
-
+	word[i] = '\0';
+	return (word);
+}
