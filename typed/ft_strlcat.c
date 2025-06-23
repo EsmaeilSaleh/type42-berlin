dd741eb7ecfedc26a88d67d9a6fdaace3294dbcc
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-23 09:51:27 +0200

    log updated

--- typed/ft_strlcat.c
+++ typed/ft_strlcat.c
@@ -15,26 +15,3 @@
 **  Function: ft_strlcat — Practice Mode: Copy / Recall
 */
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	dst_len;
-	size_t	src_len;
-
-	dst_len = 0;
-	while (dst[dst_len] && dst_len < dstsize)
-		dst_len++;
-	src_len = 0;
-	while (src[src_len])
-		src_len++;
-	if (dst_len == dstsize)
-		return (dstsize + src_len);
-	i = 0;
-	while (src[i] && (dst_len + i) < (dstsize - 1))
-	{
-		dst[dst_len + i] = src[i];
-		i++;
-	}
-	dst[dst_len + i] = '\0';
-	return (dst_len + src_len);
-}
