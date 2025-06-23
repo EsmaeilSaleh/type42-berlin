8acc8df4992729b7c7f3b7038479227c6ce9c6dc
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-23 11:20:25 +0200

    log updated

--- typed/ft_substr.c
+++ typed/ft_substr.c
@@ -24,6 +24,8 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	s_len = ft_strlen(s);
+	if(start >= s_len)
+		return (ft_strdup(""));
 	if (len > (s_len - start))
 		len = s_len - start;
 	substr = malloc(len + 1);
