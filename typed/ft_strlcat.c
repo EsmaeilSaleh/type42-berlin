21bf5760fafb1913587fd97cd35a0d233a49e0c0
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-23 10:46:59 +0200

    log updated

--- typed/ft_strlcat.c
+++ typed/ft_strlcat.c
@@ -22,8 +22,17 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	src_len;
 
 	dst_len = 0;
-	while (dst[dst_len] && dst_ln < dstsize)
+	while (dst[dst_len] && dst_len < dstsize)
 		dst_len++;
+	src_len = ft_strlen(src);
 	if (dst_len == dstsize)
 		return (dst_len + src_len);
-	src_len = ft_strlen(src);
+	i = 0;
+	while (src[i] && (dst_len + i) < (dstsize - 1))
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
+}
