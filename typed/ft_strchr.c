9f4165b7c50ec23e42e5303a9ed791865874ca98
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-23 07:19:15 +0200

    log updated

--- typed/ft_strchr.c
+++ typed/ft_strchr.c
@@ -20,9 +20,9 @@ char	*ft_strchr(const char *s, int c)
 	while (*s)
 	{
 		if (*s == (char)c)
-			return ((char *)s);
-		s++
-;	}
+			return (char *)s);
+		s++;
+	}
 	if (c == '\0')
 		return ((char *)s);
 	return (NULL);
