ca5332f379fba97bd7af9042182d7bebaef19aeb
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-23 16:04:13 +0200

    log updated

--- typed/ft_strtrim.c
+++ typed/ft_strtrim.c
@@ -22,4 +22,20 @@ char	*ft_strtrim(const char *str, const char *set)
 	size_t	start;
 	char	*trimmed;
 
-	if (
+	if (!str || !set)
+		return (NULL);
+	start = 0;
+	while (str[start] && ft_strchr(set, str[start]))
+		start++;
+	end = ft_strlen(str);
+	while (end > start && ft_strchr(set, str[end - 1]))
+		end--;
+	trimmed = malloc(end - start + 1);
+	if (!trimmed)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		trimmed[i++] = str[start++];
+	trimmed[i] = '\0';
+	return (trimmed);
+}
