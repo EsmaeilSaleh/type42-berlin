167955d7c2f1e3bc5eebaefa7cf7f157e09741dd
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-24 19:39:24 +0200

    log updated

--- typed/ft_substr.c
+++ typed/ft_substr.c
@@ -15,4 +15,16 @@
 **  Function: ft_substr — Practice Mode: Copy / Recall
 */
 
-ENd
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
