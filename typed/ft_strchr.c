b5e97b9c41ad07462a1724852cef13c373d39436
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-25 17:56:56 +0200

    log updated

--- typed/ft_strchr.c
+++ typed/ft_strchr.c
@@ -1,13 +1,29 @@
-#include <stddef.h>
-#include <stdlib.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-size_t	ft_strlen(const char *s)
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: ft_strchr — Practice Mode: Copy / Recall
+*/
+
+char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
 }
