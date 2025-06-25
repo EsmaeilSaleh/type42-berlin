91f8d8c0db7f1ec2c03fc7a3b60957e3f633ec57
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-25 18:38:23 +0200

    log updated

--- typed/ft_substr.c
+++ typed/ft_substr.c
@@ -17,14 +17,6 @@
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
-	char	*sub;
+	 char	*sub;
 	size_t	i;
-	size_t	s_len;
-
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-		return (ft_strdup(""));
-	if (len > s_len - start)
-		len = s_len - start;
+	
