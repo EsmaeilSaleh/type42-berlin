10bb754c5e1a0675d0bde971d7e0ac2a1b96725c
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-25 15:54:26 +0200

    log updated

--- typed/update_stash.c
+++ typed/update_stash.c
@@ -15,3 +15,30 @@
 **  Function: update_stash — Practice Mode: Copy / Recall
 */
 
+char	*update_stash(char *stash)
+{
+	int		i;
+	int		j;
+	char	*new_stash;
+	size_t	len;
+
+	i = 0;
+	j = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+	{
+		free(stash);
+		return (NULL);
+	}
+	len = ft_strlen(stash) - i;
+	new_stash = malloc(sizeof(char) * (len + 1));
+	if (!new_stash)
+		return (NULL);
+	i++;
+	while (stash[i])
+		new_stash[j++] = stash[i++];
+	new_stash[j] = '\0';
+	free(stash);
+	return (new_stash);
+}
