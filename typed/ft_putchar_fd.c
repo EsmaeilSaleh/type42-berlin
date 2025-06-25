c5ff42236a649e585af5e18ff9959029ebb43ec8
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-25 20:36:25 +0200

    log updated

---
+++ typed/ft_putchar_fd.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: ft_putchar_fd — Practice Mode: Copy / Recall
+*/
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
