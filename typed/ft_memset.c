c0c28d48ade9030855fd4f5916f0314c37df4d47
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-26 07:55:55 +0200

    log updated

--- typed/ft_memset.c
+++ typed/ft_memset.c
@@ -17,10 +17,15 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
+	size_t			i;
 	unsigned char	*p;
 
+	i = 0;
 	p = (unsigned char *)s;
-	while (n--)
-		*p++ = (unsigned char)c;
+	while (i < n)
+	{
+		p[i] = (unsigned char)c;
+		i++;
+	}
 	return (s);
 }
