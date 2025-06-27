642e47ef8fb6291e626ee760e7536f2dc9afd59d
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-27 19:18:05 +0200

    log updated

--- typed/ft_strjoin.c
+++ typed/ft_strjoin.c
@@ -18,26 +18,18 @@
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*joined;
-	size_t	i;
-	size_t	j;
+	char	*p;
 
 	if (!s1 || !s2)
 		return (NULL);
 	joined = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!joined)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		joined[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		joined[i + j] = s2[j];
-		j++;
-	}
-	joined[i + j] = '\0';
+	p = joined;
+	while (*s1)
+		*p++ = *s1++;
+	while (*s2)
+		*p++ = *s2++;
+	*p = '\0';
 	return (joined);
 }
