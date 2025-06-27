0a312d7f6b00cddaeafc3674f8ec6c21d08892d3
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-27 18:05:07 +0200

    log updated

--- typed/update_stash.c
+++ typed/update_stash.c
@@ -24,21 +24,4 @@ char	*update_stash(char *stash)
 
 	i = 0;
 	j = 0;
-	while (stash[i] && stash[i] != '\n')
-		i++;
-	if (!stash[i])
-	{
-		free(stash);
-		return (NULL);
-	}
-	len = ft_strlen(stash) - i;
-	new_stash = malloc(sizeof(char) * (len + 1));
-	if (!new_stash)
-		return (NULL);
-	i++;
-	while (stash[i])
-		new_stash[j++] = stash[i++];
-	new_stash[j] = '\0';
-	free(stash);
-	return (new_stash);
-}
+	while (stash[i] && stash[i] != 
