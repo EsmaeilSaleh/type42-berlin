9ebc7e6de767e5694a762e0db55b208e62fed3d1
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-28 10:35:06 +0200

    log updated

--- typed/get_next_line.c
+++ typed/get_next_line.c
@@ -15,21 +15,25 @@
 **  Function: get_next_line — Practice Mode: Copy / Recall
 */
 
-char	*get_next_line(fd)
+char	*get_next_line(int fd)
 {
 	int			bytes;
 	char		*line;
-	char		buffer[BUFFER_SIZE + 1];
+	char 		buffer[BUFFER_SIZE + 1];
 	static char	*stash;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	bytes = 1;
-	while (!ft_strchr(stash, '\n') && bytes > 0)
+	while (!ft_strchr(stash, '\0') && bytes > 0)
 	{
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes < 0)
+		{
+			free(stash);
+			stash = NULL;
 			return (NULL);
+		}
 		buffer[bytes] = '\0';
 		stash = ft_strjoin(stash, buffer);
 	}
