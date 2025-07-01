5ce2a46f75501cbcd437e89417fa31d9095fe4d1
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-07-01 09:37:44 +0200

    log updated

--- typed/main.c
+++ typed/main.c
@@ -29,9 +29,10 @@ int	main(void)
 	line = get_next_line(fd);
 	while (line)
 	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(fd);
+		printf("%s
+", line);
+	free(line);
+	line = get_next_line(fd;
 	}
 	close(fd);
 	return (0);
