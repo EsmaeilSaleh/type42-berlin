c92efa98dea2dd44ea78096bf81b1c1381630574
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-07-05 06:57:21 +0200

    log updated

--- typed/main.c
+++ typed/main.c
@@ -15,24 +15,4 @@
 **  Function: main — Practice Mode: Copy / Recall
 */
 
-int	main(void)
-{
-	int		fd;
-	char	*line;
 
-	fd = open("test.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Error opening file");
-		return (1);
-	}
-	line = get_next_line(fd);
-	while (line)
-	{
-		printf("%s", line)l
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (0);
-}
