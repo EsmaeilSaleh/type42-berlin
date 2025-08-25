7eb0c886edf1aa83b89a53c6b4406896f6947819
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-08-25 05:32:59 +0200

    log updated

--- typed/ft_putnbr_fd.c
+++ typed/ft_putnbr_fd.c
@@ -15,19 +15,3 @@
 **  Function: ft_putnbr_fd — Practice Mode: Copy / Recall
 */
 
-void	ft_putnbr_fd(int n, int fd)
-{
-	char	c;
-
-	if (n == -2147483648)
-		return (write(fd, "-2147483648", 11));
-	if (n < 0)
-	{
-		write(fd, "-", 1);
-		n = -n;
-	}
-	if (n > 9)
-		ft_putnbr_fd(n / 10, fd);
-	c = n % 10 + '0';
-	write(fd, &c, 1);
-}
