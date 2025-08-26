51c6ac436bd8551997a5525ab5e95690db392131
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-08-26 07:12:34 +0200

    log updated

--- typed/ft_itoa.c
+++ typed/ft_itoa.c
@@ -15,40 +15,8 @@
 **  Function: ft_itoa — Practice Mode: Copy / Recall
 */
 
-static int	get_int_len(int n)
+static int get_int_len(int n)
 {
 	int	len;
 
 	len = (n <= 0);
-	while (n)
-	{
-		n /= 10;
-		len++;
-	}
-	return (len);
-}
-
-char	*ft_itoa(int n)
-{
-	int	len;
-	long	nbr;
-	char	*res;
-
-	len = get_int_len(n);
-	nbr = n;
-	res = malloc(len + 1);
-	if (!res)
-		return (NULL);
-	res[len] = '\0';
-	if (nbr < 0)
-	{
-		nbr = -nbr;
-		res[0] = '-';
-	}
-	while (--len >= 0 && res[len] != '-')
-	{
-		res[len] = (nbr % 10) + '0';
-		nbr /= 10;
-	}
-	return (res);
-}
