330e83b11e805b2c2fc688170ab40716c88e6b1d
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-10-07 10:54:57 +0330

    log updated

--- typed/ft_putstr_fd.c
+++ typed/ft_putstr_fd.c
@@ -15,3 +15,16 @@
 **  Function: ft_putstr_fd — Practice Mode: Copy / Recall
 */
 
+void	ft_putstr_fd(char *s, int fd)
+{\
+	nt	t;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
