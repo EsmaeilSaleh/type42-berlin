77bf581005f0728c3921abc30769062e3574cf9a
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-11-02 15:55:52 +0100

    log updated

---
+++ typed/handle_left_align_flag.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_left_align_flag                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: handle_left_align_flag — Practice Mode: Copy / Recall
+*/
+
+int
