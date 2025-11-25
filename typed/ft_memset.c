9ab4abe19b28fdf4f6b8eba08206858488088698
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-11-25 19:58:01 +0100

    log updated

--- typed/ft_memset.c
+++ typed/ft_memset.c
@@ -15,3 +15,17 @@
 **  Function: ft_memset — Practice Mode: Copy / Recall
 */
 
+void	*ft_memset(void *s, int c , saize_t n)
+{
+	size_t		i;
+	unsigned char	*p;
+
+	i = 0;
+	p = (unsigned char *)s;
+	while (i < n)
+	{
+		p[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
