cc895ed496701fa1d0fad58ecdb934a98204022e
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-07 20:43:12 +0100

    log updated

---
+++ typed/swap_bits.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_bits                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: swap_bits — Practice Mode: Copy / Recall
+*/
+
+unsigned char 	swap_bits(unsigned char octet)
+{
+	return (octet >> 4 || octect << 4);
+}
