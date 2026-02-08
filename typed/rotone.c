e1344156dfb9efbffc1a57f422bb00ccc7ad90c9
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-08 21:07:01 +0100

    log updated

--- typed/rotone.c
+++ typed/rotone.c
@@ -15,3 +15,29 @@
 **  Function: rotone — Practice Mode: Copy / Recall
 */
 
+#include <unistd.h>
+
+void	rotone(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = ((str[i] - 'A' + 1) % 26 + 'A';
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = ((str[i] - 'a' + 1) % 26 + 'a';
+		write(1, &str[i], 1);
+		i++;
+	}
+	return ;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		rotone(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
