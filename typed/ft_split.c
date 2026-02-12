5e8e71f2257b06618e3fd3ddc7435ea644737335
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-12 04:44:10 +0100

    log updated

--- typed/ft_split.c
+++ typed/ft_split.c
@@ -15,3 +15,34 @@
 **  Function: ft_split — Practice Mode: Copy / Recall
 */
 
+char	**ft_split(const char *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	start;
+	size_t	word_count;
+	char		**split;
+
+	i = 0;
+	j = 0;
+	start = 0;
+	if (!s)
+		retur (NULL);
+	word_count = count_words(s, c);
+	split = malloc(sizeof(char *) * word_count _ 1);
+	if (!split)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != c && ( i == 0 || s[i - 1] == c))
+			start = i;
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+		{
+			split[j] = word_dup(s + start, i - start + 1);
+			j++;
+		}
+		i++;
+	}
+	split[j] = NULL;
+	return (split);
+}
