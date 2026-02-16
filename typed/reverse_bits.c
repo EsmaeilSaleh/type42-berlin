16c313de0b54e6a73057ea4d0f7efeb60fa50750
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-16 22:10:29 +0100

    log updated

--- typed/reverse_bits.c
+++ typed/reverse_bits.c
@@ -15,3 +15,17 @@
 **  Function: reverse_bits — Practice Mode: Copy / Recall
 */
 
+unsigned char	reverse_bits(unsigned char octet)
+{
+	int	i;
+	unsigned char res;
+
+	i = 8;
+	res = 0;
+	while (i--)
+	{
+		res = (res << 1) | (octet & 1);
+		octet >>=1;
+	}
+	return (res);
+}
