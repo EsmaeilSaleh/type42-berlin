1ba6016652484e5eb06b0d2a38277777504d1c2f
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-20 03:10:02 +0100

    log updated

---
+++ typed/ft_list_foreach.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: ft_list_foreach — Practice Mode: Copy / Recall
+*/
+
