d46b4f1e0e2e3cdd6cc6530444699ffbd916270b
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-20 03:12:34 +0100

    log updated

---
+++ typed/ft_list_remove_if.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_if                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 09:00:00 by esaleh            #+#    #+#             */
+/*   Updated: ✨ type42.berlin by ft trainer ✨        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**  type42-berlin — Typing Trainer for 42 Network
+**  Function: ft_list_remove_if — Practice Mode: Copy / Recall
+*/
+
