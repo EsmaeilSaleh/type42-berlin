5f59d8bb55439ab7915190c5cd58539afca3d374
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-23 18:12:34 +0100

    log updated

--- typed/ft_lstadd_front.c
+++ typed/ft_lstadd_front.c
@@ -15,11 +15,3 @@
 **  Function: ft_lstadd_front — Practice Mode: Copy / Recall
 */
 
-void	*ft_lstadd_front(t_list **lst, t_list *new)
-{
-	if (lst && new)
-	{
-		new->next = *lst;
-		*lst = new;
-	}
-}
