16f54ca5fc2c0d728bb73fb4eb559c8b26628e45
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-25 11:55:11 +0100

    log updated

--- typed/fprime.c
+++ typed/fprime.c
@@ -15,3 +15,39 @@
 **  Function: fprime — Practice Mode: Copy / Recall
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+static void	print_factor(int *first, int factor)
+{
+	if (!*first)
+		printf("*");
+	printf("%d", factor);
+	*first = 0;
+}
+
+int	main(int argc, char **argv)
+{
+	int	n;
+	int	factor;
+	int	first;
+
+	fist = 1;
+	if (argc == 2){
+	{
+		n = atoi(argv[1]);
+		factor = 2;
+		while (n > 1)
+		{
+			if (n % factor == 0)
+			{
+				print_factor(&first, factor);
+				n /= factor;
+			}
+			else
+				factor++;
+		}
+	}
+	printf("\n");
+	return (0);
+}
