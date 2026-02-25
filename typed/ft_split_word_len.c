c022aca969f5d9e3e0b0aa6b197586fba5884354
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-25 17:42:14 +0100

    log updated

--- typed/ft_split_word_len.c
+++ typed/ft_split_word_len.c
@@ -15,14 +15,3 @@
 **  Function: ft_split_word_len — Practice Mode: Copy / Recall
 */
 
-static int	is_sep(char c);
-
-static int	word_len(char *s)
-{
-		int len;
-
-		len = 0;
-		while (s[len] && !is_sep(s[len])
-			len++;
-		return (len);
-}
