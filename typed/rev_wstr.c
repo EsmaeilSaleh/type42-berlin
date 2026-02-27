55c92de100033d101670dc2419b71fa6e83344f0
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-27 17:30:56 +0100

    log updated

--- typed/rev_wstr.c
+++ typed/rev_wstr.c
@@ -15,37 +15,3 @@
 **  Function: rev_wstr — Practice Mode: Copy / Recall
 */
 
-#include <unistd.h>
-
-int	main(int argc, char **argv)
-{
-	int	i;
-	int	start;
-	int	end;
-	int	first = 1;
-
-	if (argc == 2)
-	{
-		i = 0;
-		while (argv[1][i])
-			i++;
-		while (i >= 0)
-		{
-			while (i >= 0 && (argv[1][i] == ' ' || argv[1][i] == '\t' || argv[1][i] == '\0'))
-				i--;
-			end = i;
-			while (i >= 0 && (argv[1][i] != ' ' && argv[1][i] != '\t')
-				i--;
-			start = i + 1;
-			if (start <= end)
-			{
-				if (!first)
-					write(1, " ", 1);
-				write(1, &argv[1][start], end - start + 1);
-				first = 0;
-			}
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
-}
