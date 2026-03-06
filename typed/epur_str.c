9ad1ab3abe1355f1c11567a1da520953f4a4839c
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-03-06 08:17:22 +0100

    log updated

--- typed/epur_str.c
+++ typed/epur_str.c
@@ -17,29 +17,7 @@
 
 #include <unistd.h>
 
-int	main(int 	argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int	i;
-	int	int_word;
 
-	if (argc == 2)
-	{
-		i = 0;
-		i_word = 0;
-		while (arv[1][i])
-		{
-			if (argv[1][i] != ' ' && argv[1][i] != '\t')
-			{
-				if (in_word)
-						write(1, " ", 1);
-				while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
-					write(1, &argv[1][i++], 1);
-				in_word = 1;
-			}
-			else
-				i++;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
-}
+	END
