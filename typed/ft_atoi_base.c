11b8f7050f83fa9ab6321af2292cf28d3e29bcc2
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-03-11 08:40:13 +0100

    log updated

--- typed/ft_atoi_base.c
+++ typed/ft_atoi_base.c
@@ -15,3 +15,38 @@
 **  Function: ft_atoi_base — Practice Mode: Copy / Recall
 */
 
+int	ft_atoi_base(const char *str, int str_base)
+{
+	int	res;
+	int	sign;
+	int	val;
+
+	res = 0;
+	sign = 1;
+	if (!str || str_base < 2 || str_base > 16)
+		return (0);
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str)
+	{
+		if (*str >= '0' && *str <= '9')
+			val = *str - '0';
+		if (*str >= 'a' && *str <= 'z')
+			val = *str - 'a' + 10;
+		if (*str >= 'A' && *str <= 'Z')
+			val = *str - 'A' + 10;
+		else
+			break;
+		if (val >= str_base)
+			break;
+		res = res * str_base + val;
+		str++;
+	}
+	return (res * sign);
+}
