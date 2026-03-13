b503176870f91a46e94c908fc55f109fe0bc9dd6
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-03-13 10:35:06 +0100

    log updated

--- typed/ft_atoi.c
+++ typed/ft_atoi.c
@@ -15,3 +15,25 @@
 **  Function: ft_atoi — Practice Mode: Copy / Recall
 */
 
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	long	result;
+
+	sign = 1;
+	result = 0;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str + '0');
+		str++;
+	}
+	return ((int)(result * sign));
+}
