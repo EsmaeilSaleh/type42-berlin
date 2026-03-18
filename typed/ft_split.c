aea083e80a13e82b96c6d53aa55c0e9c339be283
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-03-18 11:13:33 +0100

    log updated

--- typed/ft_split.c
+++ typed/ft_split.c
@@ -15,38 +15,4 @@
 **  Function: ft_split — Practice Mode: Copy / Recall
 */
 
-#include <stdlib.h>
-
-static int is_sep(char c);
-static int word_count (char *s)
-;static char *dup_word(char *s);
-
-char **ft_split(char *str)
-{
-	char **tab;
-	int		w;
-	int		i;
-
-	tab = (char **)malloc(sizeof(char *) * (word_count(str) + 1)
-;
-	if (!tab)
-		return (NULL);
-	i = 0;
-	w = 0;
-	while (str[i])
-	{
-		while (str[i] && is_sep(str[i]))
-			i++;
-		if (str[i])
-		{
-			tab[w] = dup_word(&str[i]);
-			if (!tab[w])
-				return (NULL);
-			while (str[i] && !is_sep(str[i])
-				i++;
-			w++;
-		}
-	}
-	tab[w] = NULL;
-	return (tab);
-}
+:q
