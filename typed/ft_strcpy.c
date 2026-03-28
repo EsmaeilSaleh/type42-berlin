61594e0eb3b7df219526f41308a4901262e3e5e9
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-03-28 22:05:38 +0100

    log updated

--- typed/ft_strcpy.c
+++ typed/ft_strcpy.c
@@ -17,15 +17,11 @@
 
 char	*ft_strcpy(char *dst, const char *src)
 {
-	size_t	i;
+	char	*ret;
 
-	i = 0;
-	while (src[i])
-	{
-		dst[i] = src[i];
-
-		i++;
-	}
-	dst[i] = '\0';
-	return (0);
+	ret = dst;
+	while (*src)
+		*dst++ = *src++;
+	*dst = '\0';
+	return (ret);
 }
