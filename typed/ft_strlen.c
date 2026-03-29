31d993df894f82157d4b8d17fb9cd06e21d8b156
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-03-29 19:55:54 +0200

    log updated

--- typed/ft_strlen.c
+++ typed/ft_strlen.c
@@ -15,12 +15,12 @@
 **  Function: ft_strlen — Practice Mode: Copy / Recall
 */
 
-int	ft_strlen(char *str)
+size_t	ft_strlen(const char *s)
 {
-	int	len;
+	const char	*start;
 
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
+	start = s;
+	while (*s0
+		s++;
+	return (s - start);
 }
