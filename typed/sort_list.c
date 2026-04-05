385cd3b9f0b23ff5a9d11f562b1270b174342a3b
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-05 20:05:25 +0200

    log updated

--- typed/sort_list.c
+++ typed/sort_list.c
@@ -15,3 +15,37 @@
 **  Function: sort_list — Practice Mode: Copy / Recall
 */
 
+static void	swapt_content(t_list *a, t_list *b)
+{
+	void	*tmp;
+
+	tmp = a->content;
+	a->content = b->content;
+	b->content = tmp;
+}
+
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+{
+	int		swapped;
+	t_list	*cur;
+
+	if (!lst)
+		return (NULL);
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		cur = lst;
+		while (cur->next)
+		{
+			if (cmp((int)(long)cur->content, 
+							(int)(long)cur->next->content) > 0)
+			{
+				swap_content(cur, cur->next);
+				swapped = 1;
+			}
+			cur = cur->next;
+		}
+	}
+	return (lst);
+}
