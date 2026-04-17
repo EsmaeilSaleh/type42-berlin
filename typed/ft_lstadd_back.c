293968cd928761b3f342d6b4b1e4ce570e613316
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-17 10:39:10 +0200

    log updated

--- typed/ft_lstadd_back.c
+++ typed/ft_lstadd_back.c
@@ -15,17 +15,3 @@
 **  Function: ft_lstadd_back — Practice Mode: Copy / Recall
 */
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list *last;
-
-	if (!lst || !new)
-		return ;
-	if (!*lst)
-	{
-		*lst = new;
-		return ;
-	}
-	last = ft_lstlast(*lst);
-	last->next = new;
-}
