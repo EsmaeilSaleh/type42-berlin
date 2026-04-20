55ea8e80aa033dfa5da4bb81484138930e94cbdc
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-04-20 20:49:16 +0200

    log updated

--- typed/ft_strncmp.c
+++ typed/ft_strncmp.c
@@ -15,4 +15,12 @@
 **  Function: ft_strncmp — Practice Mode: Copy / Recall
 */
 
-1
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (
